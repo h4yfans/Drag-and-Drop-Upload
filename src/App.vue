0464f56df00f5d92ac7f9d001f881c8430368338
Kaan Karaca <kaan94karaca@gmail.com>
2016-11-16 15:33:30 +0300

    Multiple file drag and drop uploader with Vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <hello></hello>
+    </div>
+</template>
+
+<script>
+import UploadForm from './components/UploadForm'
+
+export default {
+  name: 'app',
+  components: {
+    UploadForm
+  }
+}
+
+</script>
