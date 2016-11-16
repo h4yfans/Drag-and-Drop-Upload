7d3ad1729356962adc19a6968b1f67d5527df1b5
Kaan Karaca <kaan94karaca@gmail.com>
2016-11-16 20:07:36 +0300

    overall file progress

--- src/components/Uploads.vue
+++ src/components/Uploads.vue
@@ -3,30 +3,67 @@
         <div class="dragndrop__status" v-if="files.length">
             <ul class="list-inline">
                 <li class="list-inline__item">File: {{files.length}}</li>
-                <li class="list-inline__item">Percentage: x%</li>
+                <li class="list-inline__item">Percentage: {{overallProgress}}%</li>
                 <li class="list-inline__item list-inline__item--last">Time remaining: 00::00</li>
             </ul>
         </div>
-    <file v-for="file in files" :file="file"></file>
+        <file v-for="file in files" :file="file"></file>
     </div>
 </template>
 
 <script>
     import File from './File'
+    import eventHub from '../events.js'
     export default{
+        data(){
+            return {
+                overallProgress: 0
+            }
+        },
         props: [
             'files'
         ],
         components: {
             File
+        },
+        methods: {
+            unfinishedFile(){
+                var i, files = []
+
+                for (i = 0; i < this.files.length; i++) {
+                    if (this.files[i].finished || this.files[i].cancelled) {
+                        continue
+                    }
+
+                    files.push(this.files[i])
+                }
+
+                return files
+            },
+            updateOverallProgress(){
+                var unfinishedFiles= this.unfinishedFile(), totalProgress= 0;
+                //for each unfinished file
+                //total progress 400%
+                unfinishedFiles.forEach((file) => {
+                    totalProgress += file.progress
+                });
+                this.overallProgress = parseInt(totalProgress / unfinishedFiles.length || 0)
+                //total progress / unfinished file count
+                //otherwise 0
+            }
+        },
+        mounted(){
+            eventHub.$on('progress', (fileObject, e) => {
+                this.updateOverallProgress()
+            })
         }
     }
 </script>
 
 
 <style scoped>
-    .dragndrop__status{
+    .dragndrop__status {
         text-align: center;
-        padding:20px;
+        padding: 20px;
     }
 </style>
